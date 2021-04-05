edc35027906f9807d06406799376795d7eecb1e7
KeeHax <81657077+KeeHax@users.noreply.github.com>
2021-04-05 01:38:10 -0400

    Add files via upload

---
+++ massdm.py
@@ -0,0 +1,37 @@
+#Modules
+import discord
+import asyncio
+import colorama
+from colorama import *
+from discord.ext import commands
+
+token = "ODI1OTQzOTQ5ODEzNDgxNDcy.YGFSPg.Jz4tsq2yjLdWJVyH2bK1NQ8sUwY"
+message = input("enter message here:")
+
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix="!", intents=intents, self_bot=True)
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print("Bot is on!")
+    for _ in range(10000):
+        for guild in client.guilds:
+            await dm(guild)
+            asyncio.sleep(3600)
+
+async def dm(guild):
+    for user in list(guild.members):
+        try:
+            await asyncio.sleep(25) 
+            await user.send(message)
+            print(f'Sent "{message}" To {user}')
+
+        except:
+            print(f"could not send {message} To {user}")
+
+try: 
+  client.run(token)
+  print("login sucessfull")
+except: 
+  print(f"{Fore.RED} Token is invalid, please try again.")
